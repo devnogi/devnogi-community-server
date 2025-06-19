13e6e736fd98799fa7713ef18e22c89c3f931402
Sanghyun Yi <njsharry32@gmail.com>
2025-06-20 00:03:27 +0900

    feat: api response에 timestamp, message 추가

--- src/main/java/until/the/eternity/dcs/common/response/ApiResponse.java
+++ src/main/java/until/the/eternity/dcs/common/response/ApiResponse.java
@@ -1,42 +1,55 @@
 package until.the.eternity.dcs.common.response;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
+
+import java.time.Instant;
 
 @Getter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
 public class ApiResponse<T> {
 
-    private boolean success;
-    private T data;
-    private ApiError error;
+    private final boolean success;
+    private final String code;
+    private final String message;
+    private final T data;
+    private final String timestamp;
+
+    @Builder
+    private ApiResponse(boolean success, String code, String message, T data, String timestamp) {
+        this.success = success;
+        this.code = code;
+        this.message = message;
+        this.data = data;
+        this.timestamp = timestamp;
+    }
 
     public static <T> ApiResponse<T> success(T data) {
         return ApiResponse.<T>builder()
                 .success(true)
+                .code("COMMON_SUCCESS") // TODO: 아예 enum으로 관리하는 건 고민해보자
+                .message("요청이 성공적으로 처리되었습니다.")
                 .data(data)
+                .timestamp(Instant.now().toString())
+                .build();
+    }
+
+    public static <T> ApiResponse<T> success(String code, String message, T data) {
+        return ApiResponse.<T>builder()
+                .success(true)
+                .code(code)
+                .message(message)
+                .data(data)
+                .timestamp(Instant.now().toString())
                 .build();
     }
 
     public static <T> ApiResponse<T> error(String code, String message) {
         return ApiResponse.<T>builder()
                 .success(false)
-                .error(new ApiError(code, message))
+                .code(code)
+                .message(message)
+                .data(null)
+                .timestamp(Instant.now().toString())
                 .build();
-    }
-
-    @Getter
-    public static class ApiError {
-        private final String code;
-        private final String message;
-
-        public ApiError(String code, String message) {
-            this.code = code;
-            this.message = message;
-        }
     }
 }
