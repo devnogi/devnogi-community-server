be424e25c4c1ec72baa72607bb75cb0726d24d1e
LeeShinHeang <dragon11796@gmail.com>
2025-06-22 10:39:10 +0900

    feat: DTO에 validation, swagger 관련 어노테이션 추가

--- src/main/java/until/the/eternity/dcs/domain/board/dto/response/BoardPersistResponse.java
+++ src/main/java/until/the/eternity/dcs/domain/board/dto/response/BoardPersistResponse.java
@@ -1,9 +1,13 @@
 package until.the.eternity.dcs.domain.board.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
+
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
 
 @Builder
 public record BoardPersistResponse(
+	@Schema(description = "게시판 아이디", example = "1", requiredMode = REQUIRED)
 	Long id
 ) {
 	public static BoardPersistResponse of(Long id) {
