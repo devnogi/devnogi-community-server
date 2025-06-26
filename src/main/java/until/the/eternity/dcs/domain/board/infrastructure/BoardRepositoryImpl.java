4458360155552293c1db81ffddd0f1366837e65d
LeeShinHeang <dragon11796@gmail.com>
2025-06-26 16:42:53 +0900

    test: BoardServiceTest 구현

--- src/main/java/until/the/eternity/dcs/domain/board/infrastructure/BoardRepositoryImpl.java
+++ src/main/java/until/the/eternity/dcs/domain/board/infrastructure/BoardRepositoryImpl.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import until.the.eternity.dcs.domain.board.entity.Board;
 import until.the.eternity.dcs.domain.board.entity.BoardRepository;
-import until.the.eternity.dcs.domain.board.exception.BoardNotFoundException;
 
 import java.util.List;
 import java.util.Optional;
