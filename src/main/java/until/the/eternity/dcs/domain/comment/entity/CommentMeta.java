0e873599a6448193963324f7a7c3172861518989
LeeShinHeang <dragon11796@gmail.com>
2025-07-23 14:21:17 +0900

    refactor: comment_meta 도메인 생성

---
+++ src/main/java/until/the/eternity/dcs/domain/comment/entity/CommentMeta.java
@@ -0,0 +1,26 @@
+package until.the.eternity.dcs.domain.comment.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Table(name = "comment_meta")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EntityListeners(AuditingEntityListener.class)
+public class CommentMeta {
+    @Id private Long commentId;
+
+    @Column(name = "like_count", nullable = false)
+    private Integer likeCount = 0;
+}
