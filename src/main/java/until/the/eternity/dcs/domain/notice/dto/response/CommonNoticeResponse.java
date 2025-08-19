8c0f49e4b7692766865a0fe9cd4944565b61fbbb
LeeShinHeang <dragon11796@gmail.com>
2025-08-19 10:19:00 +0900

    feat: 알림용 공통응답구조 생성

---
+++ src/main/java/until/the/eternity/dcs/domain/notice/dto/response/CommonNoticeResponse.java
@@ -0,0 +1,75 @@
+package until.the.eternity.dcs.domain.notice.dto.response;
+
+import java.time.LocalDateTime;
+import lombok.Builder;
+import until.the.eternity.dcs.domain.notice.entity.Notice;
+
+@Builder
+public record CommonNoticeResponse(
+        Long id,
+        Long userId,
+        String title,
+        String contents,
+        String url,
+        LocalDateTime createdAt,
+        Boolean isRead) {
+    public static CommonNoticeResponse from(Notice notice) {
+        String contents = buildContents(notice);
+
+        return CommonNoticeResponse.builder()
+                .id(notice.getId())
+                .userId(notice.getUserId())
+                .title(notice.getTitle())
+                .contents(contents)
+                .url(notice.getUrl())
+                .createdAt(notice.getCreatedAt())
+                .isRead(notice.getIsRead())
+                .build();
+    }
+
+    private static String buildContents(Notice notice) {
+        switch (notice.getNoticeType()) {
+            case POST_LIKE:
+                {
+                    return "회원님의 게시글에 좋아요가 달렸습니다.";
+                }
+            case POST_COMMENT:
+                {
+                    return "회원님의 게시글에 댓글이 달렸습니다.";
+                }
+            case COMMENT_REPLY:
+                {
+                    return "회원님의 댓글에 답글이 달렸습니다.";
+                }
+            case COMMENT_LIKE:
+                {
+                    return "회원님의 댓글에 좋아요가 달렸습니다.";
+                }
+            case ANNOUNCEMENT:
+                {
+                    return "새로운 공지가 게시되었습니다.";
+                }
+            case SYSTEM:
+                {
+                    return "시스템 알림입니다.";
+                }
+            case EVENT:
+                {
+                    return "새로운 이벤트가 게시되었습니다.";
+                }
+            case REPORT_RESULT:
+                {
+                    return "회원님의 신고가 처리되었습니다.";
+                }
+            case POST_BLOCKED:
+                {
+                    return "회원님의 게시글이 제재되었습니다.";
+                }
+            case COMMENT_BLOCKED:
+                {
+                    return "회원님의 댓글이 제재되었습니다.";
+                }
+        }
+        return "기타 알림";
+    }
+}
