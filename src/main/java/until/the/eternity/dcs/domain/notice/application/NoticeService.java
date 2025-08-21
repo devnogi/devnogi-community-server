3f7888b7a6fface41dcc59b0b8b87473931c6464
LeeShinHeang <dragon11796@gmail.com>
2025-08-21 09:01:49 +0900

    feat: 엔드포인트 생성

--- src/main/java/until/the/eternity/dcs/domain/notice/application/NoticeService.java
+++ src/main/java/until/the/eternity/dcs/domain/notice/application/NoticeService.java
@@ -1,7 +1,11 @@
 package until.the.eternity.dcs.domain.notice.application;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import until.the.eternity.dcs.domain.notice.dto.request.NoticeSendRequest;
+import until.the.eternity.dcs.domain.notice.dto.response.NoticeCommonResponse;
+import until.the.eternity.dcs.domain.notice.dto.response.NoticePersistResponse;
 import until.the.eternity.dcs.domain.notice.entity.NoticeRepository;
 
 @Service
@@ -9,8 +13,29 @@ import until.the.eternity.dcs.domain.notice.entity.NoticeRepository;
 public class NoticeService {
     private final NoticeRepository noticeRepository;
 
-    // 알림 생성
+    public NoticePersistResponse createNotice(NoticeSendRequest noticeSendRequest) {
+        // 생성
 
-    // 알림 확인 상태 변경
+        // 저장
 
+        return null;
+    }
+
+    public NoticeCommonResponse getDetailNotice(Long id) {
+        // 조회
+
+        // isRead 업데이트
+
+        // 리턴
+
+        return null;
+    }
+
+    public List<NoticeCommonResponse> getNoticeList(Integer day) {
+        // 조회 (최근 day일 데이터만)
+
+        // 리턴
+
+        return null;
+    }
 }
--- src/main/java/until/the/eternity/dcs/domain/notice/presentation/NoticeController.java
+++ src/main/java/until/the/eternity/dcs/domain/notice/presentation/NoticeController.java
@@ -1,9 +1,21 @@
 package until.the.eternity.dcs.domain.notice.presentation;
 
+import static org.springframework.http.HttpStatus.CREATED;
+
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import until.the.eternity.dcs.domain.notice.application.NoticeService;
+import until.the.eternity.dcs.domain.notice.dto.request.NoticeSendRequest;
+import until.the.eternity.dcs.domain.notice.dto.response.NoticeCommonResponse;
+import until.the.eternity.dcs.domain.notice.dto.response.NoticePersistResponse;
 
 @RestController
 @RequiredArgsConstructor
@@ -11,9 +23,21 @@ import until.the.eternity.dcs.domain.notice.application.NoticeService;
 public class NoticeController {
     private final NoticeService noticeService;
 
-    // 알림 전송 API
+    @PostMapping
+    public ResponseEntity<NoticePersistResponse> sendNotice(
+            @RequestBody NoticeSendRequest noticeSendRequest) {
+        NoticePersistResponse response = noticeService.createNotice(noticeSendRequest);
+        return ResponseEntity.status(CREATED).body(response);
+    }
 
-    // 알림 확인 API
+    @GetMapping("/{id}")
+    public NoticeCommonResponse getDetailNotice(@PathVariable Long id) {
+        return noticeService.getDetailNotice(id);
+    }
 
+    @GetMapping
+    public List<NoticeCommonResponse> getNoticeList(@RequestParam("day") Integer day) {
+        return noticeService.getNoticeList(day);
+    }
 }
 // todo 스웨거
