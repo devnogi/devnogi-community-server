09b9c1ddcafea86a41743dd3a24958bcea969fe1
bsko98 <bsko98@naver.com>
2025-11-10 12:57:06 +0900

    chore: Redisson config파일 추가

---
+++ src/main/java/until/the/eternity/dcs/common/config/RedissonConfig.java
@@ -0,0 +1,40 @@
+package until.the.eternity.dcs.common.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedissonConfig {
+    @Value("${spring.data.redis.host:localhost}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port:6379}")
+    private int redisPort;
+
+    @Value("${spring.data.redis.password:}")
+    private String redisPassword;
+
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+
+        String address = String.format("redis://%s:%d", redisHost, redisPort);
+
+        config.useSingleServer()
+                .setAddress(address)
+                .setPassword(redisPassword.isEmpty() ? null : redisPassword)
+                .setConnectionPoolSize(64)
+                .setConnectionMinimumIdleSize(10)
+                .setTimeout(3000)
+                .setRetryAttempts(3)
+                .setRetryInterval(1500)
+                .setKeepAlive(true)
+                .setTcpNoDelay(true);
+
+        return Redisson.create(config);
+    }
+}
