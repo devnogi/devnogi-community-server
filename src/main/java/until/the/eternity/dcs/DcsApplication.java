b4024ece23dd7a58e4ba0aa5128a28c838f961b2
bsko98 <bsko98@naver.com>
2025-11-16 00:48:29 +0900

    chore: Redisson 제거

--- src/main/java/until/the/eternity/dcs/DcsApplication.java
+++ src/main/java/until/the/eternity/dcs/DcsApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(exclude = {org.redisson.spring.starter.RedissonAutoConfigurationV2.class})
+@SpringBootApplication
 @EnableJpaAuditing
 @EnableScheduling
 public class DcsApplication {
