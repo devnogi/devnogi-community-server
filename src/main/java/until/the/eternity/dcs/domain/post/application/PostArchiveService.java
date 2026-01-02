8edd6fc3da6fdb4ea5b25139f4b3e2e4b5e9bb2a
bsko98 <bsko98@naver.com>
2026-01-02 20:38:24 +0900

    feat: post 삭제후 아카이빙 테이블로 이관시 해당 post에 첨부된 이미지를 삭제하는 로직 구현

--- src/main/java/until/the/eternity/dcs/domain/post/application/PostArchiveService.java
+++ src/main/java/until/the/eternity/dcs/domain/post/application/PostArchiveService.java
@@ -2,12 +2,14 @@ package until.the.eternity.dcs.domain.post.application;
 
 import jakarta.transaction.Transactional;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import until.the.eternity.dcs.domain.post.entity.Post;
 import until.the.eternity.dcs.domain.post.entity.PostArchive;
+import until.the.eternity.dcs.domain.post.entity.PostImage;
 import until.the.eternity.dcs.domain.post.infrastructure.PostArchiveRepository;
 import until.the.eternity.dcs.domain.post.infrastructure.PostLikeRepository;
 import until.the.eternity.dcs.domain.post.infrastructure.PostMetaRepository;
@@ -23,6 +25,7 @@ public class PostArchiveService {
     private final PostLikeRepository postLikeRepository;
     private final PostMetaRepository postMetaRepository;
     private final PostMetaService postMetaService;
+    private final MinioService minioService;
 
     @Transactional
     public void archiveOldPost(LocalDateTime date) {
@@ -36,10 +39,24 @@ public class PostArchiveService {
         postTagRepository.deleteAllByPostIdIn(postIdList);
         postLikeRepository.deleteAllByPostIdIn(postIdList);
         postMetaRepository.deleteAllByPostIdIn(postIdList);
+        List<String> filesToDelete = new ArrayList<>();
+        for (Post post : postList) {
+            filesToDelete =
+                    post.getImages().stream()
+                            .map(PostImage::getStoredFileName)
+                            .collect(Collectors.toList());
+        }
         for (Long postId : postIdList) {
             postMetaService.deletePostMeta(postId);
         }
         postRepository.deleteAllByIdIn(postIdList);
         postArchiveRepository.saveAll(postArchiveList);
+        for (String fileName : filesToDelete) {
+            try {
+                minioService.deleteFile(fileName);
+            } catch (Exception e) {
+                throw new RuntimeException("이미지를 삭제할 수 없습니다.", e);
+            }
+        }
     }
 }
