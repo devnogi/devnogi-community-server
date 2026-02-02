dfb9a650a2f6d5cf48da15eaa908f2f334617ac8
dev-ant <njsharry32@gmail.com>
2026-02-03 08:37:31 +0900

    feat: allow origin pattern 게이트웨이와 통일

--- src/main/java/until/the/eternity/dcs/common/filter/JwtHeaderFilter.java
+++ src/main/java/until/the/eternity/dcs/common/filter/JwtHeaderFilter.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -19,6 +20,7 @@ import until.the.eternity.dcs.common.entity.CustomWebAuthenticationDetails;
 import until.the.eternity.dcs.common.util.IpAddressUtil;
 import until.the.eternity.dcs.domain.user.enums.UserGrade;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class JwtHeaderFilter extends OncePerRequestFilter {
@@ -35,10 +37,14 @@ public class JwtHeaderFilter extends OncePerRequestFilter {
         String userIdCode = request.getHeader("X-USER-ID");
         String userGradeCode = request.getHeader("X-USER-GRADE");
 
+        log.debug("JWT Header Parsing - X-USER-ID: {}, X-USER-GRADE: {}", userIdCode, userGradeCode);
+
         UsernamePasswordAuthenticationToken authentication =
                 getAuthentication(userIdCode, userGradeCode);
 
         String clientIp = ipAddressUtil.getClientIp(request);
+
+        log.debug("Client IP: {}", clientIp);
 
         CustomWebAuthenticationDetails webAuthenticationDetails =
                 new CustomWebAuthenticationDetails(request, clientIp);
@@ -46,6 +52,9 @@ public class JwtHeaderFilter extends OncePerRequestFilter {
         authentication.setDetails(webAuthenticationDetails);
 
         SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        log.debug("Authentication set - Principal: {}, Authorities: {}",
+                authentication.getPrincipal(), authentication.getAuthorities());
 
         filterChain.doFilter(request, response);
     }
@@ -55,13 +64,17 @@ public class JwtHeaderFilter extends OncePerRequestFilter {
         Long userId;
         try {
             userId = Long.parseLong(userIdCode);
+            log.debug("Parsed userId: {}", userId);
         } catch (NumberFormatException e) {
+            log.debug("Failed to parse userIdCode: '{}', setting userId to null", userIdCode);
             userId = null;
         }
         if (userGradeCode == null) {
+            log.debug("userGradeCode is null, returning authentication without authorities");
             return new UsernamePasswordAuthenticationToken(userId, null);
         }
         UserGrade userGrade = UserGrade.fromCode(userGradeCode).orElse(null);
+        log.debug("Parsed userGrade: {} from code: {}", userGrade, userGradeCode);
         List<GrantedAuthority> authorities = new ArrayList<>();
         authorities.add(new SimpleGrantedAuthority("ROLE_" + userGrade.toString()));
         return new UsernamePasswordAuthenticationToken(userId, null, authorities);
