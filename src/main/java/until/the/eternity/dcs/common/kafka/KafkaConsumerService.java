494af88ae520268ced997f7e1dbf801cad8ceb94
bsko98 <bsko98@naver.com>
2026-02-20 00:23:32 +0900

    feat: 로컬 및 운영 환경(test-topic)에서 테스트를 위한 세팅 추가

---
+++ src/main/java/until/the/eternity/dcs/common/kafka/KafkaConsumerService.java
@@ -0,0 +1,16 @@
+package until.the.eternity.dcs.common.kafka;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class KafkaConsumerService {
+    @KafkaListener(topics = "test-topic", groupId = "devnogi-community-group")
+    public void consume(TestDTO message) {
+        log.info("컨수머 테스트 시작");
+        log.info("🎉 Kafka로부터 받은 메시지: {}", message);
+        log.info("컨수머 종료");
+    }
+}
