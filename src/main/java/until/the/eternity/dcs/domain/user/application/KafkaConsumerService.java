d13eed0717f0dc57340966484969e24a0fc42a71
bsko98 <bsko98@naver.com>
2026-02-28 11:09:43 +0900

    chore: kafka로부터 받은 데이터 검증 로직 추가 및 DLQ 추가로 문제가 생긴 메시지들 로깅하도록 설정

--- src/main/java/until/the/eternity/dcs/domain/user/application/KafkaConsumerService.java
+++ src/main/java/until/the/eternity/dcs/domain/user/application/KafkaConsumerService.java
@@ -1,5 +1,6 @@
 package until.the.eternity.dcs.domain.user.application;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -14,7 +15,7 @@ public class KafkaConsumerService {
     private final UserSummaryRepository userSummaryRepository;
 
     @KafkaListener(topics = "USER_INFO_UPDATE_EVENT", groupId = "devnogi-community-group")
-    public void consume(UserSummaryConsumerDTO userInfo) {
+    public void consume(@Valid UserSummaryConsumerDTO userInfo) {
         log.info("Kafka로부터 받은 user 데이터: {}", userInfo);
         userSummaryRepository.save(userInfo.toEntity());
     }
