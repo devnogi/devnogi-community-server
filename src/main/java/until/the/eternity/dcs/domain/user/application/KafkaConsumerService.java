394d5ea7daf7f8b420fe6490f7e901af0ad62fb5
bsko98 <bsko98@naver.com>
2026-02-28 10:42:20 +0900

    chore: auth 서버로부터 데이터를 받아올 때 Redis를 사용하지 않도록 변경

--- src/main/java/until/the/eternity/dcs/domain/user/application/KafkaConsumerService.java
+++ src/main/java/until/the/eternity/dcs/domain/user/application/KafkaConsumerService.java
@@ -1,32 +1,21 @@
 package until.the.eternity.dcs.domain.user.application;
 
-import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import until.the.eternity.dcs.domain.user.dto.response.UserSummaryConsumerDTO;
-import until.the.eternity.dcs.domain.user.entity.UserSummary;
+import until.the.eternity.dcs.domain.user.infrastructure.UserSummaryRepository;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class KafkaConsumerService {
-    private final RedisTemplate<String, Object> redisTemplate;
-    private static final String UNPROCESSED_QUEUE_KEY = "usersummary:queue:unprocessed";
+    private final UserSummaryRepository userSummaryRepository;
 
     @KafkaListener(topics = "USER_INFO_UPDATE_EVENT", groupId = "devnogi-community-group")
     public void consume(UserSummaryConsumerDTO userInfo) {
         log.info("Kafka로부터 받은 user 데이터: {}", userInfo);
-        String uniqueId = String.valueOf(userInfo.id());
-        String dataKey = "temp:user:data" + uniqueId;
-        UserSummary newUser = userInfo.toEntity();
-        redisTemplate.opsForValue().set(dataKey, newUser, 24, TimeUnit.HOURS);
-
-        redisTemplate.opsForList().rightPush(UNPROCESSED_QUEUE_KEY, uniqueId);
-
-        log.info("Redis 개별 저장 (24h TTL) 및 큐 등록 완료: ID={}", uniqueId);
-        log.info("Redis 개별 저장 (24h TTL) 및 큐 등록 완료: data= {}", newUser.toString());
+        userSummaryRepository.save(userInfo.toEntity());
     }
 }
